2f7675d1bd2e24b0fd0997a4f759ecde4c9eb0bf
Denis Rybin <drybin@gmail.com>
2026-04-20 22:58:06 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,34 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	runes := []rune(s)
+	var outString string
+	for i := 0; i < len(runes); i++ {
+		currentChar := runes[i]
+
+		switch {
+		case currentChar > 47 && currentChar < 58 && i == 0: // первая цифра в строке
+			return "", ErrInvalidString
+		case currentChar > 47 && currentChar < 58 && runes[i-1] > 47 && runes[i-1] < 58: // предыдущий символ тоже цифра
+			return "", ErrInvalidString
+		case currentChar == 48: // zero
+			outString = string([]rune(outString)[:len([]rune(outString))-1])
+			continue
+		case currentChar > 48 && currentChar < 58: // цифра от 1 до 9
+			digit, _ := strconv.Atoi(string(currentChar))
+			for x := 0; x < digit-1; x++ {
+				outString += string(runes[i-1])
+			}
+			continue
+		}
+
+		outString += string(currentChar)
+	}
+	return outString, nil
 }
